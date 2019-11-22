7810d2ca5128d905b1fc0c66cb605ee390a52df5
krzyczak <krzyczak@users.noreply.github.com>
2019-11-22 12:05:11 +0000

    First Java solution

--- Java/src/main/java/parrot/Parrot.java
+++ Java/src/main/java/parrot/Parrot.java
@@ -1,42 +1,87 @@
 package parrot;
 
-public class Parrot {
+import java.util.HashMap;
 
-    private ParrotTypeEnum type;
-    private int numberOfCoconuts;
-    private double voltage;
-    private boolean isNailed;
-
-    public Parrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
-        this.type = type;
-        this.numberOfCoconuts = numberOfCoconuts;
-        this.voltage = voltage;
-        this.isNailed = isNailed;
+abstract class Parrot {
+  public static Parrot getParrotOld(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
+    switch (type) {
+      case EUROPEAN:
+        return new European(numberOfCoconuts, voltage, isNailed);
+      case AFRICAN:
+        return new African(numberOfCoconuts, voltage, isNailed);
+      case NORWEGIAN_BLUE:
+        return new NorwegianBlue(numberOfCoconuts, voltage, isNailed);
+      default:
+        throw new RuntimeException("Should be unreachable");
     }
+  }
 
-    public double getSpeed() {
-        switch (type) {
-            case EUROPEAN:
-                return getBaseSpeed();
-            case AFRICAN:
-                return Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
-            case NORWEGIAN_BLUE:
-                return (isNailed) ? 0 : getBaseSpeed(voltage);
-            default:
-                throw new RuntimeException("Should be unreachable");
-        }
-    }
+  public static HashMap<String, Class<? extends Parrot>> parrotTypes;
+  static {
+      parrotTypes = new HashMap<>();
+      parrotTypes.put("EUROPEAN", European.class);
+      parrotTypes.put("AFRICAN", African.class);
+      parrotTypes.put("NORWEGIAN_BLUE", NorwegianBlue.class);
+  }
 
-    private double getBaseSpeed(double voltage) {
-        return Math.min(24.0, voltage * getBaseSpeed());
-    }
+  public static Parrot getParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
+    // Parrot parrot = (Parrot)parrotTypes.get(type.toString()).getDeclaredConstructors()[0].newInstance(numberOfCoconuts, voltage, isNailed);
+    Parrot parrot = getParrotOld(type, numberOfCoconuts, voltage, isNailed);
 
-    private double getLoadFactor() {
-        return 9.0;
-    }
+    return parrot;
+  }
 
-    private double getBaseSpeed() {
-        return 12.0;
-    }
+  protected int numberOfCoconuts;
+  protected double voltage;
+  protected boolean isNailed;
 
+  public Parrot(int numberOfCoconuts, double voltage, boolean isNailed) {
+      this.numberOfCoconuts = numberOfCoconuts;
+      this.voltage = voltage;
+      this.isNailed = isNailed;
+  }
+
+  protected double getBaseSpeed(double voltage) {
+    return Math.min(24.0, voltage * getBaseSpeed());
+  }
+
+  protected double getLoadFactor() {
+    return 9.0;
+  }
+
+  protected double getBaseSpeed() {
+    return 12.0;
+  }
+
+  public abstract double getSpeed();
+}
+
+class European extends Parrot {
+  public European(int numberOfCoconuts, double voltage, boolean isNailed) {
+    super(numberOfCoconuts, voltage, isNailed);
+  }
+
+  public double getSpeed() {
+    return getBaseSpeed();
+  }
+}
+
+class African extends Parrot {
+  public African(int numberOfCoconuts, double voltage, boolean isNailed) {
+    super(numberOfCoconuts, voltage, isNailed);
+  }
+
+  public double getSpeed() {
+    return Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
+  }
+}
+
+class NorwegianBlue extends Parrot {
+  public NorwegianBlue(int numberOfCoconuts, double voltage, boolean isNailed) {
+    super(numberOfCoconuts, voltage, isNailed);
+  }
+
+  public double getSpeed() {
+    return (isNailed) ? 0 : getBaseSpeed(voltage);
+  }
 }
